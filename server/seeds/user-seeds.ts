c5a6093709cee5321fbcc3f48450acea3c18e138
JStevens <stevens38@marshall.edu>
2024-11-26 19:17:40 -0500

    user login starter code

---
+++ server/seeds/user-seeds.ts
@@ -0,0 +1,9 @@
+import { User } from '../models/user.js';
+
+export const seedUsers = async () => {
+  await User.bulkCreate([
+    { username: 'Jonathan', password: 'password' },
+    { username: 'Maverick', password: 'password' },
+    { username: 'Ed', password: 'password' },
+  ], { individualHooks: true });
+};
