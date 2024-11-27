5ad087f74beaaed79bd2ddd573a313c4f4d8072b
Maverick S <maverickmmsc@gmail.com>
2024-11-26 19:11:44 -0500

    made a base format for the header,footer, and navs. along with updates for the main and app tsx's

---
+++ client/src/components/Snav.tsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+//this is the nav for those logged into the app
+export default function Snav() {
+    return (
+        <div>
+            <Link to="/">Home</Link>
+            <Link to="/readlist">Read List</Link>
+            <Link to="/wantlist">Want to Read</Link>
+        </div>
+    )
+}
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,42 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './index.css'
+
+import App from './App.tsx'
+import Homepage from './pages/homepage.tsx'
+import Readlist from './pages/readlist.tsx'
+import Wantlist from './pages/wantlist.tsx'
+import ErrorPage from './pages/errorpage.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Homepage />,
+      },
+      {
+        path: '/readlist',
+        element: <Readlist />,
+      },
+      {
+        path: '/wantlist',
+        element: <Wantlist />,
+      },
+    ],
+  },
+]);
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <RouterProvider router={router}/>
+  );
+} else {
+  console.error('Failed to find the root element');
+}
