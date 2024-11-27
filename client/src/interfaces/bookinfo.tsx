0c5885592221c5f417e6900548c58ed5da8c682d
Maverick S <maverickmmsc@gmail.com>
2024-11-26 19:25:38 -0500

    interfaces added

---
+++ client/src/interfaces/bookinfo.tsx
@@ -0,0 +1,13 @@
+
+interface BookInfo {
+    id: number;
+    title: string;
+    book_cover: string;
+    length: number;
+    author: string;
+    genre: string;
+    rating: number;
+    description: string;
+}
+
+export default BookInfo;
