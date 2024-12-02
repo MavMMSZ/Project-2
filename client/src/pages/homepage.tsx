b6f57000deed2965014cd952d005bee6ac44ef3b
Edward Ofori <tetraed.llc@gmail.com>
2024-12-01 23:12:46 -0500

    Boostrap

--- client/src/pages/homepage.tsx
+++ client/src/pages/homepage.tsx
@@ -1,16 +1,14 @@
 import BooksList from "../components/Home";
-// import '../styles/homepage.css';
-// import '../styles/carousel.css';
-import Carousel from "../components/carousel";
+import '../styles/homepage.css';
+import '../styles/carousel.css';
+
 const Homepage = () => {
-  const bookUrl = "https://example.com/book-image.jpg"; // Replace with the actual book image URL from the BookList component
+  
 
   return (
     <div>
       <h1>Welcome To the Home of Your Favorite Books</h1>
-      <div>
-        <Carousel bookUrl={BooksList.booksData.imageUrl} />
-      </div>
+      
       <BooksList />
     </div>
   );
