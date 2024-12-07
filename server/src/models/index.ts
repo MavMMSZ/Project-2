b10692704233820b30746d67e94eaf67417f510c
Maverick S <maverickmmsc@gmail.com>
2024-12-06 19:08:04 -0500

    we'll see

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -8,6 +8,16 @@ console.log('DB_NAME:', process.env.DB_NAME);
 console.log('DB_USER:', process.env.DB_USER);
 console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
 
+// const sequelize = process.env.DB_URL
+//   ? new Sequelize(process.env.DB_URL)
+//   : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
+//     host: 'localhost',
+//     dialect: 'postgres',
+//     dialectOptions: {
+//       decimalNumbers: true,
+//     },
+//   });
+// incase this shit dosent work go to knban models/index
 const sequelize = new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD || '', {
   host: 'localhost',
   dialect: 'postgres',
@@ -18,6 +28,5 @@ sequelize.authenticate()
   .catch((error) => console.error('Unable to connect to the database:', error));
 
 const User = UserFactory(sequelize);
-
 
 export { sequelize, User };
